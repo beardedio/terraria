4b69568842afb262d30cca09e71784614820ac40
Chris Locker <5058266-chrisjoj@users.noreply.gitlab.com>
2021-04-02 15:03:38 +0100

    Handle SIGTERM by saving world and shutting down gracefully

--- template/vanilla/run-vanilla.sh
+++ template/vanilla/run-vanilla.sh
@@ -27,5 +27,42 @@ if [ "${world:-null}" != null ]; then
     CMD="$CMD -world /config/$world"
 fi
 
+# Create named pipe if one doesn't already exist
+# This is so we can send input to TerrariaServer
+if [ ! -p "/vanilla/console" ]; then
+    mkfifo /vanilla/console
+fi
+
+# trap SIGTERM signal and call graceful_shutdown
+trap 'kill ${!}; graceful_shutdown' SIGTERM
+
+function graceful_shutdown() {
+    # Send a message to players that the server is shutting down
+    echo say 'Server shutting down' > /vanilla/console
+
+    echo "Stopping Terraria server..."
+    echo "Saving world"
+
+    echo exit  > /vanilla/console
+
+    # Waiting for server to finish saving & shutting down
+    pid=$(pgrep -f ^./TerrariaServer)
+    if [ -z "$pid" ]; then exit 1; fi
+    while [ -e /proc/$pid ]; do
+        sleep 1
+    done
+    echo "World save complete"
+    echo "Shutting down"
+    exit 0
+}
+
 echo "Starting container, CMD: $CMD $@"
-exec $CMD $@
+(tail -f > /vanilla/console & $CMD $@ < console) &
+
+tail -f /dev/null & wait ${!}
+
+pid=$(pgrep -f ^./TerrariaServer)
+if [ -z "$pid" ]; then exit 1; fi
+while [ -e /proc/$pid ]; do
+   sleep 5
+done
